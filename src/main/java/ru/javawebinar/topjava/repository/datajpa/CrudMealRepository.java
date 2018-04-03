cf7fc312876978c22002b111cf27b81137c8af09
AlexShnaider <Shnaider.sasha@gmail.com>
2018-04-03 15:04:44 +0700

    HW05 DataJPA, spring profiles, tests for each profile

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,32 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Modifying
+    @Transactional
+    int deleteByUserIdAndId(int userId, int id);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Transactional
+    Optional<Meal> findByUserIdAndId(int userId, int id);
+
+    @Transactional
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(int userId);
+
+    @Transactional
+    List<Meal> findAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(
+            int userId, LocalDateTime startDate, LocalDateTime endDate);
 }
